d21ddb5f0451e465b7687ed3b4d28c8c571cb3e4
c-h-276 <cameron.hord@wsu.edu>
2025-12-09 11:26:29 -0800

    edited image js again and copied original css

--- major-project/first-meeting.js
+++ major-project/first-meeting.js
@@ -22,8 +22,11 @@ window.onload = () => {
     const c2 = document.getElementById("choice2");
     const c3 = document.getElementById("choice3");
 
-    sceneImage.style.width = "auto";
-    sceneImage.style.height = "100%";
+    sceneImage.style.display = "block";
+    sceneImage.style.margin = "0 auto";
+    sceneImage.style.width = "100%";
+    sceneImage.style.height = "auto";
+    sceneImage.style.maxHeight = "100%";
     sceneImage.style.objectFit = "contain";
 
     // hide choices by default
