4d62276a6e0038bd263e73e49f4862ae37411905
c-h-276 <cameron.hord@wsu.edu>
2025-12-09 12:32:27 -0800

    edited css and js

--- major-project/beginning.js
+++ major-project/beginning.js
@@ -29,13 +29,6 @@ window.onload = () => {
         const button = document.getElementById("nextbtn");
         const sceneImage = document.getElementById("StartingScreen");
 
-        sceneImage.style.display = "block";
-        sceneImage.style.margin = "0 auto";
-        sceneImage.style.width = "100%";
-        sceneImage.style.height = "auto";
-        sceneImage.style.maxHeight = "100%";
-        sceneImage.style.objectFit = "contain";
-
         let index = 0;
         textElement.textContent = messages[index];
         sceneImage.src = images[index];
